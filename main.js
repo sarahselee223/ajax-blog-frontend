dc4f3b60dfef6bd634acc8d4e1fb10a2bc42ba3c
Sarah Lee <sarahselee223@gmail.com>
2018-11-25 16:47:38 -0800

    added get all, get one, update, delete, create - need to fix url bug

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 
 
-function createFunction(){
+function createDisplay(){
     const createPost = document.querySelector('.createPost')
+    const showPost = document.querySelector('.showPost')
     createPost.style.display = "block"
+    showPost.style.display = "none"
+    event.preventDefault()
 }
 
-function getPosting(){
+function getPosts(){
+    const showPost = document.querySelector('.showPost')
+    showPost.style.display = "block"
+
     axios.get('http://127.0.0.1:5000/posts/')
         .then(function(result){
             displayPosts(result.data)
         })
 }
+
+const list = document.querySelector('.list-group')
+list.onclick = function(event){
+    const address = event.target.href
+    getOnePost(address.slice(30,39))
+}
+
+function getOnePost(id){
+    axios.get(`http://127.0.0.1:5000/posts/${id}`)
+        .then(function(result){
+            displayPosts([result.data])
+        })
+}
+
 function displayPosts (postArr){
+    if(postArr.length <= 1){
+        const post = postArr[0]
+        const title = post.title
+        const content = post.content
+        getContent(title, content)
+    } else {
     postArr.map(post => {
         const id = post.id
         const title = post.title
@@ -19,6 +45,7 @@ function displayPosts (postArr){
         getTitle(id, title)
         getContent(title, content)
     })
+    }
 }
     
 function getTitle(id, title){
@@ -33,8 +60,6 @@ function getTitle(id, title){
     titleList.appendChild(li)
 }
 
-
-
 function getContent(title, content){
     const titleDisplay = document.getElementById("title-display")
     const nestedTitle = document.getElementById('nested-title')
@@ -42,7 +67,8 @@ function getContent(title, content){
     const h2 = document.createElement('h2')
     h2.setAttribute("id", "nested-title")
     const titleNode = document.createTextNode(title)
-    titleDisplay.appendChild(titleNode)
+    h2.appendChild(titleNode)
+    titleDisplay.appendChild(h2)
     
     const contentDisplay = document.getElementById("content-display")
     const nestedContent = document.getElementById('nested-content')
@@ -50,16 +76,97 @@ function getContent(title, content){
     const p = document.createElement('p')
     p.setAttribute("id", "nested-content")
     const contentNode = document.createTextNode(content)
-    contentDisplay.appendChild(contentNode)
+    p.appendChild(contentNode)
+    contentDisplay.appendChild(p)
 }
 
-
-
-function registerFunction(event){
-    
+function postRegister(event){
     const createPost = document.querySelector('.createPost')
-    createPost.style.display = "none"
     const showPost = document.querySelector('.showPost')
+    const newTitle = document.getElementById('title').value
+    const newContent = document.getElementById('content').value
+    createNewPost(newTitle, newContent)
+    createPost.style.display = "none"
     showPost.style.display = "block"
     event.preventDefault()
 }
+
+function createNewPost (newTitle, newContent){
+    axios.post('http://127.0.0.1:5000/posts/', {
+        "title": newTitle,
+        "content": newContent
+    })
+    .then(function(result){
+        const id = result.data.id
+        window.location.href = `/#/posts/${id}`
+        reset()
+    })
+}
+
+const deletePost = document.getElementById('delete-post')
+deletePost.onclick = function(e){
+    delteOnePosting(window.location.href)
+}
+
+function delteOnePosting(address){
+    const id = address.slice(30,39)
+    axios.delete(`http://127.0.0.1:5000/posts/${id}`)
+        .then(function(result){
+            reset()
+        })
+}
+
+function reset(){
+    const titleList = document.querySelector('.list-group')
+    titleList.innerHTML=""
+    getPosts()
+}
+
+const editPostButton = document.getElementById('edit-post')
+editPostButton.onclick = function(e){
+    const showPost = document.querySelector('.showPost')
+    showPost.style.display = "none"
+    const editPost = document.querySelector('.editView')
+    editPost.style.display = "block"
+    const id = window.location.href.slice(30,39)
+    fetchEditFormData(id)
+}
+
+function fetchEditFormData(id){
+    axios.get(`http://127.0.0.1:5000/posts/${id}`)
+    .then(function(result){
+        populateEditForm(result.data.id, result.data.title, result.data.content)
+    })
+}
+
+function populateEditForm(id, title, content){
+    window.location.href = `/#/posts/${id}`
+    const titleLocation = document.querySelector('.editView #title')
+    const contentLocation = document.querySelector('.editView #content')
+    titleLocation.value = title
+    contentLocation.value = content  
+}
+
+function updateFunction(event){
+    event.preventDefault()
+    const id = window.location.href.slice(30,39)
+    const title= document.querySelector('.editView #title').value
+    const content = document.querySelector('.editView #content').value
+    updatePost(id, title, content)
+}
+
+function updatePost(id, title, content){
+
+    axios.put(`http://127.0.0.1:5000/posts/${id}`,{
+            "title": title,
+            "content": content
+    })
+        .then(function(result){
+            const editPost = document.querySelector('.editView')
+            editPost.style.display = "none"
+            reset()
+            
+            getOnePosting(id)
+        })
+}
+
